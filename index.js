a35978794ff8017c1ad258307d5d5989e963e2a4
nguyenm2151 <70943380+nguyenm2151@users.noreply.github.com>
2021-03-21 21:35:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let map, infoWindow;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
-    zoom: 4,
+    zoom: 10,
     mapId: 'ebe91929550d491c'
   });
   infoWindow = new google.maps.InfoWindow();
